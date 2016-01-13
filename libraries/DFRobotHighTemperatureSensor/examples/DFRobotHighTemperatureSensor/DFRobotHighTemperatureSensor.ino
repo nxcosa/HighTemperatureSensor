9b4b8833707369a31945c14d156263eeb9471c16
nxcosa <664036851@qq.com>
2016-01-13 16:55:51 +0800

    LIB

--- libraries/DFRobotHighTemperatureSensor/examples/DFRobotHighTemperatureSensor/DFRobotHighTemperatureSensor.ino
+++ libraries/DFRobotHighTemperatureSensor/examples/DFRobotHighTemperatureSensor/DFRobotHighTemperatureSensor.ino
@@ -1,3 +1,18 @@
+/***************************************************
+ This example reads HighTemperature Sensor.
+ 
+ Created 2016-1-13
+ By berinie Chen <bernie.chen@dfrobot.com>
+ 
+ GNU Lesser General Public License.
+ See <http://www.gnu.org/licenses/> for details.
+ All above must be included in any redistribution
+ ****************************************************/
+
+/***********Notice and Trouble shooting***************
+ 1.Connection and Diagram can be found here  https://www.dfrobot.com/wiki/index.php?title=HighTemperatureSensor_SKU:SEN0198
+ 2.This code is tested on Arduino Uno.
+ ****************************************************/
 #include<DFRobotHighTemperatureSensor.h>
 
 const float voltageRef = 5.000; //Set reference voltage,you need test your IOREF voltage. 
@@ -14,4 +29,3 @@ void loop(void) {
   Serial.println(temperature);
   delay(1000); //just here to slow down the output so it is easier to read
 }
-
